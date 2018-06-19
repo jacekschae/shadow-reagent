6476fd66d4a4b36118ff8c69766af9fe992c778c
Jacek Schae <jacek.schae@gmail.com>
2018-06-19 16:57:22 +0200

    refactor to dev/after-loaf

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,7 +1,12 @@
 (ns app.core
-  (:require [reagent.core :as reagent :refer [atom]]
+  (:require [reagent.core :as r]
             [app.views :as views]))
 
-(defn ^:export main []
-  (reagent/render-component [views/app]
-                            (.getElementById js/document "app")))
+(defn ^:dev/after-load start
+  []
+  (r/render-component [views/app]
+                      (.getElementById js/document "app")))
+
+(defn ^:export main
+  []
+  (start))
